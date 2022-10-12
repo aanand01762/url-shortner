803409b15d18895c235ee9ef02f7792324edfe73
Arun Kumar <aanand01762@gmail.com>
2022-10-12 16:32:02 +0530

    Add explanation for encoding logic
    Signed-off-by: Arun Kumar <aanand01762@gmail.com>

--- pkg/shortner/shortner.go
+++ pkg/shortner/shortner.go
@@ -1,40 +1,55 @@
 package shortner
 
 type URLService struct {
-	elements string
-	COUNTER  int
-	LTOS     map[string]int
-	STOL     map[int]string
+	elements  string
+	COUNTER   int
+	LONGTOID  map[string]int
+	IDTOSMALL map[int]string
 }
 
 func (s *URLService) LongToShort(url string) (string, int, bool) {
 	var shorturl string
 	var existing bool
-	if s.LTOS == nil {
-		s.LTOS = map[string]int{}
-		s.STOL = map[int]string{}
+
+	//Check if instance struct type does not exits,
+	//then only initialise an instance which means its first entry
+	if s.LONGTOID == nil {
+		s.LONGTOID = map[string]int{}
+		s.IDTOSMALL = map[int]string{}
 		s.elements = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 		s.COUNTER = 1000000000
 	}
-	if id, ok := s.LTOS[url]; ok {
-		shorturl = s.STOL[id]
+
+	//Check if url already exists, update existing flag
+	if id, ok := s.LONGTOID[url]; ok {
+		shorturl = s.IDTOSMALL[id]
 		existing = ok
 	} else {
+
+		//if url does not exist convert id or counter
+		//of url to base62 encoded text
 		shorturl = s.base10ToBase62(s.COUNTER)
 		s.COUNTER++
-		s.LTOS[url] = s.COUNTER
-		s.STOL[s.COUNTER] = shorturl
+		s.LONGTOID[url] = s.COUNTER
+		s.IDTOSMALL[s.COUNTER] = shorturl
 	}
+
+	//Append the base62 encoded text to tinyurl
 	return "http://tiny.url/" + shorturl, s.COUNTER, existing
 }
 
 func (s *URLService) ShortToLong(url string) string {
+
+	//Return long url from IDTOSMALL hashmap using
+	//encoded text as key
 	url = url[len("http://tiny.url/"):]
 	var n int = s.base62ToBase10(url)
-	return s.STOL[n]
+	return s.IDTOSMALL[n]
 }
 
 func (s *URLService) base62ToBase10(str string) int {
+
+	//Convert base62 format to interger value in base10 format
 	var n int = 0
 	for i := 0; i < len(str); i++ {
 		n = n*62 + s.convert(str[i])
@@ -43,6 +58,8 @@ func (s *URLService) base62ToBase10(str string) int {
 }
 
 func (s *URLService) convert(c byte) int {
+
+	//Return base10 integer representation of a char in base62
 	if c >= '0' && c <= '9' {
 		return int(c - '0')
 	}
@@ -56,6 +73,8 @@ func (s *URLService) convert(c byte) int {
 }
 
 func (s *URLService) base10ToBase62(n int) string {
+
+	//Convert base62 format to interger value in base10 format
 	var sb string
 	for n != 0 {
 		sb = string(s.elements[n%62]) + sb
